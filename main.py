c527160ae36a4ea99560f57c4796d821db7c8d70
Tajallah Sims <tajallah.sims@gmail.com>
2018-07-05 02:31:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as beausu
 
-dicc = urlopen('http://www.dictionary.com/')
+class wotd:
+        word = ''
+        alt_spellings = ''
+        pos = []
+        defs = []
 
-soup = beausu(dicc, 'html.parser')
+        def __init__():
+                dicc = urlopen('http://www.dictionary.com/')
+                soup = beausu(dicc, 'html.parser')
 
-wotd = soup.find(id='wotdAudio-play').get('href')
+                wotd = soup.find(id='wotdAudio-play').get('href')
 
-dicc = urlopen(wotd)
+                dicc = urlopen(wotd)
 
-soup = beausu(dicc, 'html.parser')
+                soup = beausu(dicc, 'html.parser')
 
-word = soup.h1.text
-alt_spellings = soup.h3.text
-pos = [item.text for item in soup.find_all(class_='luna-pos')]
-defs = [item.text for item in soup.find_all(class_='css-9sn2pa e10vl5dg6')]
+                self.word = soup.h1.text
+                self.alt_spellings = soup.h3.text
+                self.pos = [item.text for item in soup.find_all(class_='luna-pos')]
+                self.defs = [item.text for item in soup.find_all(class_='css-9sn2pa e10vl5dg6')]
 
-print(word)
-print(alt_spellings)
-print(pos)
-print(defs)
+        def format():
+                output = ''
+                output += '**' + word + '**\n\n'
+                output += '[' + alt_spellings + ']\n\n'
+                for item in pos:
+                        output += item + '\n' #todo find cases of multiple POS and add handling for them
+                for item in defs:
+                        output += '*\'' + item +'*\'\n'
+                return output
